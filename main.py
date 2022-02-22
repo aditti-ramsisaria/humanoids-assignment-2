0013b960ebb10a1159d941be107e0dde62c5096f
aditti-ramsisaria <81064425+aditti-ramsisaria@users.noreply.github.com>
2022-02-21 22:13:28 -0500

    tuned for 7 emotions

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 '''
 Author: Aditti Ramsisaria
 Description: Predict emotion from speech audio trained on RAVDESS
+
 Filename identifiers for .wav files:
     Modality (01 = full-AV, 02 = video-only, 03 = audio-only).
     Vocal channel (01 = speech, 02 = song).
     Emotion (01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry, 
              06 = fearful, 07 = disgust, 08 = surprised).
     Emotional intensity (01 = normal, 02 = strong). 
-    NOTE: There is no strong intensity for the ‘neutral’ emotion.
     Statement (01 = “Kids are talking by the door”, 
                02 = “Dogs are sitting by the door”).
     Repetition (01 = 1st repetition, 02 = 2nd repetition).
@@ -18,7 +18,7 @@ Filename identifiers for .wav files:
 import time
 import librosa
 import soundfile
-import os, glob, pickle
+import os, glob
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
@@ -45,8 +45,7 @@ emotions = {
 }
 
 # subset of emotions used
-observed_emotions = ["calm", "happy", "fearful", "disgust"]
-
+observed_emotions = {"sad", "calm", "happy", "fearful", "disgust", "surprise", "angry"}
 print("Emotions subset: ", observed_emotions)
 
 # extract features from audio - mfcc, mel, chroma
@@ -61,17 +60,14 @@ def extract_feature(file_name, mfcc, chroma, mel):
         if mfcc:
             mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, 
                                                  n_mfcc=40).T, axis=0)
-            # append to result
             result = np.hstack((result, mfccs))
         if chroma:
             chroma = np.mean(librosa.feature.chroma_stft(S=stft, 
                                                 sr=sample_rate).T,axis=0)
-            # append to result
             result = np.hstack((result, chroma))
         if mel:
             mel = np.mean(librosa.feature.melspectrogram(y=X, 
                                                 sr=sample_rate).T,axis=0)
-            # append to result
             result = np.hstack((result, mel))
     return result
 
@@ -109,25 +105,71 @@ print("Number of training samples: ", x_train.shape)
 print("Number of testing samples: ", x_test.shape)
 
 # generate MLP classifier model
-# if using adam, learning_rate='adaptive', epsilon=1e-08,
-# learning_rate_init=0.0001, max_iter=5000
-optimizer = "lbfgs"
-model = MLPClassifier(solver=optimizer, alpha=0.1, 
-                      hidden_layer_sizes=(300,), 
-                      random_state=9)
+optimizer = "adam"
+hidden_layers = (300, 300, 100)
 
-# train the model
-start_time = time.time()
-model.fit(x_train, y_train)
-print("\nOptimizer used: ", optimizer)
-print("Time taken for training: ", time.time() - start_time)
-print("Loss computed: ", model.loss_)
+if optimizer == "lbfgs":
+    model = MLPClassifier(solver=optimizer, alpha=0.1, 
+                    hidden_layer_sizes=hidden_layers, 
+                    random_state=9, max_iter=5000)
 
-# predict for test set
-y_pred = model.predict(x_test)
+    # train the model
+    start_time = time.time()
+    model.fit(x_train, y_train)
+    print("\nOptimizer used: ", optimizer)
+    print("Network architecture: ", hidden_layers)
+    print("Time taken for training: ", time.time() - start_time)
+    print("Loss computed: ", model.loss_)
 
-# calculate accuracy
-accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
-print("Testing Accuracy: {:.2f}%".format(accuracy * 100))
+    # predict for test set
+    y_pred = model.predict(x_test)
 
-        
+    # calculate accuracy
+    accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+    print("Testing Accuracy: {:.2f}%".format(accuracy * 100))
+
+else:
+    # if optimizer is adam
+    N_EPOCHS = 50
+    N_CLASSES = np.unique(y_train)
+
+    scores_train = []
+    scores_test = []
+    model = MLPClassifier(solver=optimizer, alpha=0.1, 
+                          hidden_layer_sizes=hidden_layers, 
+                          random_state=9, max_iter=5000,
+                          epsilon=1e-08,
+                          learning_rate_init=0.0001,
+                          batch_size=16,
+                          learning_rate='adaptive')
+    start_time = time.time()
+
+    epoch = 0
+    while epoch < N_EPOCHS:
+        model.partial_fit(x_train, y_train, classes=N_CLASSES)
+        # SCORE TRAIN
+        scores_train.append(model.score(x_train, y_train))
+        # SCORE TEST
+        scores_test.append(model.score(x_test, y_test))
+
+        epoch += 1
+
+    print("\nOptimizer used: ", optimizer)
+    print("Network architecture: ", hidden_layers)
+    print("Time taken for training: ", time.time() - start_time)
+
+    # plot data
+    fig, ax = plt.subplots(2, sharex=True, sharey=True)
+    ax[0].plot(scores_train)
+    ax[0].set_title('Train')
+    ax[1].plot(scores_test)
+    ax[1].set_title('Test')
+    fig.suptitle("Accuracy over epochs", fontsize=14)
+    plt.show()
+
+    # predict for test set
+    y_pred = model.predict(x_test)
+
+    # calculate accuracy
+    accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+    print("Testing Accuracy: {:.2f}%".format(accuracy * 100))
